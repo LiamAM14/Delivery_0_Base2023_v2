74f3cb5e3004ce9203dd6ae1f3bdf175d5755986
wadzetma <wadzetma@alumnos.unex.es>
2023-03-20 10:52:19 +0100

    Monitores implementados, falta implementar cocina y semaforo para moteros

--- src/delivery/Cocina.java
+++ src/delivery/Cocina.java
@@ -1,21 +1,52 @@
 package delivery;
+
 import pcd.util.ColoresConsola;
 import pcd.util.Traza;
 
-public class Cocina {
-	Restaurante r;
-	
-	public Cocina (Restaurante _r) {
-		r=_r;
-	}
-	
-	public void cocinar (Pedido p) {
-		Traza.traza(ColoresConsola.GREEN, 2, "Cocinando el pedido: "+p.printConRetorno());
-		try {
-			Thread.sleep(100);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+import java.util.List;
+
+public class Cocina implements Runnable {
+    Restaurante r;
+    List<Producto> pedidoCocina;
+    BufferPan bufferPan;
+    BufferPollo bufferPollo;
+    int pan;
+    int pollo;
+
+    public Cocina(Restaurante _r, BufferPan bPan, BufferPollo bPollo) {
+        bufferPan = bPan;
+        bufferPollo = bPollo;
+        r = _r;
+    }
+
+    public void cocinar(Pedido p) {
+        pedidoCocina = p.getProductos();
+        Traza.traza(ColoresConsola.GREEN, 2, "Cocinando el pedido: " + p.printConRetorno());
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            if (pedidoCocina != null) {
+                for (Producto producto : pedidoCocina) {
+                    if (producto.getId().equals("" + 0)) {
+                        try {
+                            pan = bufferPan.extraer();
+                            pollo = bufferPollo.extraer();
+                            System.out.println("Hamburguesa cocinada con pan:" + pan + " y pollo:" + pollo);
+                        } catch (Exception e) {
+                        }
+                    }
+                }
+                break;
+            }
+        }
+    }
+
 }
--- src/delivery/Restaurante.java
+++ src/delivery/Restaurante.java
@@ -4,40 +4,46 @@ import bank.*;
 import pcd.util.ColoresConsola;
 import pcd.util.Traza;
 
- public class Restaurante{
-	private String nombre;					// nombre del restaurante
-	private Account account;				// cuenta bancaria para registrar la recaudación
-	private Cocina cocina; 					// la cocina de este restaurante
-	private ControlMoteros controlMoteros;  // los moteros de este restaurante
-
-	
-	public Restaurante (Account _ac, String _nombre, int _numeroMoteros) {
-		account = _ac;
-		nombre = _nombre;
-		controlMoteros = new ControlMoteros (this, _numeroMoteros);
-		cocina = new Cocina (this);
-		Traza.traza(ColoresConsola.GREEN_BOLD_BRIGHT, 1,"Creando restaurante: "+nombre);
-	}
+public class Restaurante {
+    BufferPollo bufferPollo = new BufferPollo();
+    BufferPan bufferPan = new BufferPan();
+    private String nombre;                    // nombre del restaurante
+    private Account account;                // cuenta bancaria para registrar la recaudación
+    private Cocina cocina;                    // la cocina de este restaurante
+    private ControlMoteros controlMoteros;  // los moteros de este restaurante
 
 
-	 public String getNombre () {
-		return nombre;
-	}
-	
-	public double getBalance (){
-		return account.getBalance();
-	}
-	
-	public Account getAccount () {
-		return account;
-	}
-	
-	public void tramitarPedido (Pedido _p) {
-		// Tramitar un pedido es:
-		System.out.println("hola");
-		account.deposit(_p.getPrecioPedido()); 	// añadir la cantidad abonada a la cuenta del banco
-		controlMoteros.asignarMotero();
-		cocina.cocinar(_p);						// mandar el pedido a cocina
-		controlMoteros.enviarPedido(_p);		// una vez cocinado, mandarlo a los moteros para que uno lo coja
-	}
+    public Restaurante(Account _ac, String _nombre, int _numeroMoteros) {
+        account = _ac;
+        nombre = _nombre;
+        controlMoteros = new ControlMoteros(this, _numeroMoteros);
+        cocina = new Cocina(this, bufferPan, bufferPollo);
+        new Thread(cocina).start();
+        new Thread(new BotPan(1, bufferPan)).start();
+        new Thread(new BotPollo(2, bufferPollo)).start();
+        Traza.traza(ColoresConsola.GREEN_BOLD_BRIGHT, 1, "Creando restaurante: " + nombre);
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getBalance() {
+        return account.getBalance();
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void tramitarPedido(Pedido _p) {
+        // Tramitar un pedido es:
+        System.out.println("hola");
+        account.deposit(_p.getPrecioPedido());    // añadir la cantidad abonada a la cuenta del banco
+        controlMoteros.asignarMotero();
+        cocina.cocinar(_p);
+        notifyAll();// mandar el pedido a cocina
+        controlMoteros.enviarPedido(_p);        // una vez cocinado, mandarlo a los moteros para que uno lo coja
+    }
 }
